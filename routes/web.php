ca13da121b5cdcc0e3c23d392359cb5b6318f641
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2020-12-13 15:29:15 +0900

    ルート情報の追加

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+/** メインページ */
+Route::get('/main', 'PrefectureController@main');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+/** ログイン済みのメインページ */
+Route::get('/login_main', 'PrefectureController@login_main')->middleware('auth');
+
+/** ユーザー情報ページ */
+Route::get('/user_inf', 'UserController@user_inf')->middleware('auth');
+
+/** ユーザー情報変更ページ */
+Route::get('/user_edit', 'UpdateController@user_edit')->middleware('auth');
+Route::post('/user_up', 'UpdateController@user_up')->middleware('auth');
