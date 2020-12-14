478271452bf9cc0d3d5509d08f96caffd43fc760
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2020-12-14 22:21:57 +0900

    都道府県情報の取得処理の作成

--- app/Models/Prefecture.php
+++ app/Models/Prefecture.php
@@ -3,13 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Prefecture extends Model
 {
     /**ガードするフィールド */
-    protected $guarded = array('prefecture_id');
+    protected $table = 'prefectures';
+    protected $guarded = array('id');
+
     /**
-     * prefecturesテーブルとusersテーブルをリレーション
+     * 都道府県情報の取得
      * @return void
      */
+    public function getData()
+    {
+        $data = DB::table($this->table)->get();
+        return $data;
+    }
 }
