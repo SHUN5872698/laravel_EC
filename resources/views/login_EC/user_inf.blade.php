b7171fdbe447fedd5d2b1a71681096ad62a78ed5
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2020-12-14 21:11:36 +0900

    レイアウトの修正

--- resources/views/login_EC/user_inf.blade.php
+++ resources/views/login_EC/user_inf.blade.php
@@ -3,60 +3,63 @@
 @section('menubar')
 
 @section('content')
-  <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-10">
-                <div class="card">
-                    <div class="card-header">ユーザー情報確認</div>
-                    <div class="card-body">
-                      <div class="form-group row-my-2">
-                        <table class="table">
-                          @foreach ($users as $user)
-                          @endforeach
-                          <tr>
-                            <th>ユーザー名:</th>
-                            <th>{{$user->name}}</th>
-                          </tr>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">ユーザー情報確認</div>
+                <div class="card-body">
+                    <div class="form-group row">
+                        <div class="col-1"></div>
+                        <div class="col-10">
+                            <table class="table table-borderless">
+                                @foreach ($users as $user)
+                                @endforeach
+                                <tr>
+                                    <th>ユーザー名:</th>
+                                    <th>{{$user->name}}</th>
+                                </tr>
 
-                          <tr>
-                            <th>メールアドレス</th>
-                            <th>{{$user->email }}</th>
-                          </tr>
-                          <tr>
-                            <th>パスワード</th>
-                            <th>******</th>
-                          </tr>
+                                <tr>
+                                    <th>メールアドレス</th>
+                                    <th>{{$user->email }}</th>
+                                </tr>
+                                <tr>
+                                    <th>パスワード</th>
+                                    <th>******</th>
+                                </tr>
 
-                          <tr>
-                            <th>電話番号</th>
-                            <th>{{$user->phone}}</th>
-                          </tr>
+                                <tr>
+                                    <th>電話番号</th>
+                                    <th>{{$user->phone}}</th>
+                                </tr>
 
-                          <tr>
-                            <th>郵便番号</th>
-                            <th>{{ $user->postcode}}</th>
-                          </tr>
+                                <tr>
+                                    <th>郵便番号</th>
+                                    <th>{{ $user->postcode}}</th>
+                                </tr>
 
-                          <tr>
-                            <th>登録住所</th>
-                            <th>{{$user->prefectures_name}} {{$user->city}} {{$user->block}} {{$user->building}}</th>
-                          </tr>
+                                <tr>
+                                    <th>登録住所</th>
+                                    <th>{{$user->prefectures_name}} {{$user->city}} {{$user->block}} {{$user->building}}</th>
+                                </tr>
 
-                          <tr>
-                            <th></th>
-                            <td></td>
-                          </tr>
-                        </table>
-                                <div class="col-md-6 offset-md-4">
-                                    <a class="btn btn-primary" href="{{ url('/user_edit') }}"role="button">ユーザー情報を変更</a>
-                                    <a class="btn btn-outline-primary" href="{{ url('/login_main') }}"role="button">キャンセル</a>
-                                </div>
-                            </div>
+                                <tr>
+                                    <th></th>
+                                    <td></td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="col-md-6 offset-md-4">
+                            <a class="btn btn-primary" href="{{ url('/user_edit') }}" role="button">ユーザー情報を変更</a>
+                            <a class="btn btn-outline-primary" href="{{ url('/login_main') }}" role="button">キャンセル</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-@endsection
+    </div>
+    @endsection
 
-@yield('footer')
-@endsection
+    @yield('footer')
+    @endsection
