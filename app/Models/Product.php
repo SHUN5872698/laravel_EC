49d5942b64f43c9950fde04c909dc6c5ba399945
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2020-12-19 01:07:36 +0900

    productimagesとのリレーション処理の作成

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,45 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use app\Models\Productimage;
 
 class Product extends Model
 {
-    //
+    /**ガードするフィールド */
+    protected $guarded = array('id');
+
+    /**
+     * productimagesテーブルとのリレーション
+     * @return void
+     */
+    public function productimage()
+    {
+        return $this->hasMany('App\Models\Productimage', 'product_id', 'image', 'kubun');
+    }
+
+    /**
+     * 商品情報と画像データの取得処理
+     */
+    public function getData()
+    {
+        $data = Product::with('productimages')
+            ->join('productimages', 'product_id',  '=', 'products.id')
+            ->where('products.id', 1)
+            ->select(
+                'products.id',
+                'products.name',
+                'products.description',
+                'products.price',
+                'products.category_master',
+                'products.category',
+                'products.capacity',
+                'productimages.product_id',
+                'productimages.image',
+                'productimages.kubun',
+            )
+            ->paginate(10);
+        $data = [
+            'data' => $data,
+        ];
+    }
 }
