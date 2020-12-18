1059b057ef1dbcdc7256b60351b3d06c39812063
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2020-12-19 01:08:08 +0900

    不要な処理の削除

--- app/Http/Controllers/PrefectureController.php
+++ app/Http/Controllers/PrefectureController.php
@@ -7,15 +7,5 @@ use App\Models\User;
 
 class PrefectureController extends Controller
 {
-    /**メインページに移動 */
-    public function main(Request $request)
-    {
-        return view('EC.main');
-    }
-
-    /**ログイン済みのメインページに移動 */
-    public function login_main(Request $request)
-    {
-        return view('login_EC.login_main');
-    }
+  //
 }
