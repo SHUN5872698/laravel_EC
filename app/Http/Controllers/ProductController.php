81fe7949cff26bbc3f3a733372a3fab56a9d0611
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2020-12-19 01:06:54 +0900

    不要な処理の削除

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class ProductController extends Controller
 {
-    //
+    /**メインページに移動 */
+    public function main(Request $request)
+    {
+        return view('EC.main');
+    }
+
+    /**ログイン済みのメインページに移動 */
+    public function login_main(Request $request)
+    {
+        return view('login_EC.login_main');
+    }
 }
