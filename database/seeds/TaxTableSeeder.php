f8f3818217f844d9883d298a1e3ec798af9ed829
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2020-12-22 12:02:08 +0900

    税率用のseederファイル作成

---
+++ database/seeds/TaxTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TaxTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('tax')->insert([
+            /**3% 1989-04-01~1997-03-31*/
+            [
+                'id' => 1,
+                'percentage' => 3,
+                'from' => date('1989-04-01')
+            ],
+
+            /**5% 1997-04-01~2014-03-31*/
+            [
+                'id' => 2,
+                'percentage' => 5,
+                'from' => date('1997-04-01')
+            ],
+
+            /**8% 2014-04-01~2019-09-30*/
+            [
+                'id' => 3,
+                'percentage' => 8,
+                'from' => date('2014-04-01')
+            ],
+
+            /**10% 2019-10-01~現在*/
+            [
+                'id' => 4,
+                'percentage' => 10,
+                'from' => date('2019-10-01')
+            ],
+        ]);
+    }
+}
