0c5e706f49bf7161f937597d84d9bfc539eb0405
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2020-12-29 23:28:06 +0900

    ログイン済みの検索結果ページの作成

---
+++ resources/views/login_EC/login_search.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.login_ec')
+@yield('css')
+@section('title', 'ログイン済み検索結果')
+@section('menubar')
+
+@section('content')
+<h1>ログイン済み検索結果</h1>
+<div class="row">
+    <div class="col-md-3">
+        <div class="card ml-3 mt-3">
+            <div class="card-header">詳細検索</div>
+            <ul class="list-group list-group-flush">
+                <text class="ml-3 mt-2">機種別で選ぶ</text>
+                <li class="list-group-item">
+                    @foreach ($categorys as $category)
+                    <a href="/login/details/category?category={{$category->category}}&category_master={{$category->category_master}}">{{$category->category}}</a>
+                    <br>
+                    @endforeach</li>
+            </ul>
+        </div>
+    </div>
+    <div class="col-md-9">
+        <div class="row">
+            @foreach ($products as $product)
+            <div class="col-md-4 my-3">
+                <div class="card">
+                    <img src="../images/{{$product->image}}" class="card-img-top my-3" alt="..." width="300" height="300">
+                    <div class="card-body">
+                        <p>
+                            <font size="2"><a href="/login/product?id={{$product->id}}&category_master={{$product->category_master}}">{{$product->name}}</a></font>
+                            <br>
+                            価格:
+                            <font size="3" color="red">
+                                ¥{{number_format($product->price * $tax->percentage)}}</font>
+                            <font size="2" color="red">(税込)</font>
+                            <br>
+                            スペック:
+                            <a href="/login/details/category?category={{$product->category}}&category_master={{$product->category_master}}"> {{$product->category}}</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        {{$products->appends(request()->query())->links()}}
+    </div>
+</div>
+
+@endsection
+@yield('footer')
+@endsection
