706a2f082181ef34ac969e03dbcab402a173284d
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2020-12-30 12:36:30 +0900

    ログイン済みメインページのリダイレクト先の変更

--- resources/views/layouts/login_ec.blade.php
+++ resources/views/layouts/login_ec.blade.php
@@ -29,7 +29,7 @@
     @section('menubar')
     <div class="container-md-">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="{{ url('/login_main') }}">Smart Shop</a>
+            <a class="navbar-brand" href="{{ url('/login/main') }}">Smart Shop</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
