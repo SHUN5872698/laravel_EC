7d0a58dd78f7b479b1da3ee0521fe9b2e7c9cac2
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2020-12-31 15:31:04 +0900

    リレーション処理の作成

--- app/Models/Cart_item.php
+++ app/Models/Cart_item.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Cart_item extends Model
 {
-    //
+    /**ガードするフィールド */
+    protected $guarded = array('id');
+
+    /**
+     * usersテーブルとのリレーション
+     * @return void
+     */
+    public function user()
+    {
+        return $this->hasMany('App\User', 'id');
+    }
+    /**
+     * productsテーブルとのリレーション
+     * @return void
+     */
+    public function product()
+    {
+        return $this->hasMany('App\Models\Product', 'id', 'name', 'description', 'price', 'category_master', 'category', 'capacity');
+    }
+    /**
+     * productimagesテーブルとのリレーション
+     * @return void
+     */
+    public function productimage()
+    {
+        return $this->hasMany('App\Models\Productimage', 'product_id', 'image', 'kubun');
+    }
 }
