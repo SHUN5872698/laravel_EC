5fecd4ad748a1f1387f68655f93038d8cbd0d542
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-01-01 12:03:12 +0900

    リレーション処理の変更

--- app/Models/Cart_item.php
+++ app/Models/Cart_item.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Cart_item extends Model
 {
@@ -16,7 +17,7 @@ class Cart_item extends Model
      */
     public function user()
     {
-        return $this->hasMany('App\User', 'id');
+        return $this->hasone('App\User', 'id');
     }
     /**
      * productsテーブルとのリレーション
@@ -33,5 +34,31 @@ class Cart_item extends Model
     public function productimage()
     {
         return $this->hasMany('App\Models\Productimage', 'product_id', 'image', 'kubun');
+    }
+
+    /**カート情報の取得 */
+    public function getCart_items(Request $request)
+    {
+        $cart_items = Cart_item::with('User')
+            ->join('users', 'users.id', '=', 'cart_items.user_id')
+            ->join('products', 'products.id', '=', 'cart_items.product_id')
+            ->join('productimages', 'productimages.product_id', '=', 'products.id')
+            ->where('cart_items.user_id', Auth::user()->id)
+            ->where('productimages.kubun', 'main')
+            ->select(
+                'cart_items.id',
+                'cart_items.user_id',
+                'cart_items.product_id',
+                'cart_items.count',
+                'products.name',
+                'products.price',
+                'products.category_master',
+                'products.category',
+                'products.capacity',
+                'productimages.image',
+            )
+            ->orderby('cart_items.id', 'asc')
+            ->paginate(12);
+        return $cart_items;
     }
 }
