66731a37cca0ba6ce9e9e08b3dfee51753b0ff65
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-01-01 14:54:57 +0900

    レイアウトの修正

--- resources/views/login_EC/user_inf.blade.php
+++ resources/views/login_EC/user_inf.blade.php
@@ -3,63 +3,63 @@
 @section('menubar')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <div class="card">
-                <div class="card-header">ユーザー情報確認</div>
-                <div class="card-body">
-                    <div class="form-group row">
-                        <div class="col-1"></div>
-                        <div class="col-10">
-                            <table class="table table-borderless">
-                                @foreach ($users as $user)
-                                @endforeach
-                                <tr>
-                                    <th>ユーザー名:</th>
-                                    <th>{{$user->name}}</th>
-                                </tr>
+<div class="row">
+    <div class="col-3 ml-3">
+        <div class="card">
+            <div class="card-header">
+                コンテンツ
+            </div>
+            <div class="card-body">
 
-                                <tr>
-                                    <th>メールアドレス</th>
-                                    <th>{{$user->email }}</th>
-                                </tr>
-                                <tr>
-                                    <th>パスワード</th>
-                                    <th>******</th>
-                                </tr>
-
-                                <tr>
-                                    <th>電話番号</th>
-                                    <th>{{$user->phone}}</th>
-                                </tr>
-
-                                <tr>
-                                    <th>郵便番号</th>
-                                    <th>{{ $user->postcode}}</th>
-                                </tr>
-
-                                <tr>
-                                    <th>登録住所</th>
-                                    <th>{{$user->prefectures_name}} {{$user->city}} {{$user->block}} {{$user->building}}</th>
-                                </tr>
-
-                                <tr>
-                                    <th></th>
-                                    <td></td>
-                                </tr>
-                            </table>
-                        </div>
-                        <div class="col-md-6 offset-md-4">
-                            <a class="btn btn-primary" href="{{ url('/user_edit') }}" role="button">ユーザー情報を変更</a>
-                            <a class="btn btn-outline-primary" href="{{ url('/login_main') }}" role="button">キャンセル</a>
-                        </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">ユーザー情報確認</div>
+            <div class="card-body">
+                <div class="form-group row justify-content-center">
+                    <div class="col-12">
+                        <table class="table table-border">
+                            @foreach ($users as $user)
+                            @endforeach
+                            <tr>
+                                <th>ユーザー名:</th>
+                                <th>{{$user->name}}</th>
+                            </tr>
+                            <tr>
+                                <th>メールアドレス</th>
+                                <th>{{$user->email }}</th>
+                            </tr>
+                            <tr>
+                                <th>電話番号</th>
+                                <th>{{$user->phone}}</th>
+                            </tr>
+                            <tr>
+                                <th>郵便番号</th>
+                                <th>{{ $user->postcode}}</th>
+                            </tr>
+                            <tr>
+                                <th>登録住所</th>
+                                <th>{{$user->prefectures_name}} {{$user->city}} {{$user->block}} {{$user->building}}</th>
+                            <tr>
+                                <th></th>
+                                <td></td>
+                            </tr>
+                        </table>
                     </div>
+
+                    <a class="btn btn-primary mx-3" href="{{ url('/user_edit') }}" role="button">ユーザー情報を変更</a>
+                    <a class="btn btn-outline-primary mx-3" href="{{ url('/login_main') }}" role="button">キャンセル</a>
                 </div>
             </div>
         </div>
     </div>
-    @endsection
+</div>
+</div>
+<div class="col-3"></div>
+</div>
+@endsection
 
-    @yield('footer')
-    @endsection
+@yield('footer')
+@endsection
