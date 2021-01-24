53e474f4db8bd9f5b7998bb72977a0f5432cb457
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-01-24 16:29:03 +0900

    ユーザー名の変更処理の作成

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,24 +7,51 @@ use Illuminate\Support\Facades\Auth;
 use App\User;
 use App\Models\Prefecture;
 
+
+
 class UserController extends Controller
 {
     /**
      * ユーザー情報と都道府県情報を取得して確認ページへ渡す
-     *
-     * @param Request $request
-     * @return void
      */
     public function user_inf()
     {
         //Userモデルからユーザ情報の取得
-        $users = new User();
-        $users = $users->User_Data();
+        $user = new User();
+        $user = $user->User_Data();
 
-        $data = [
-            'users' => $users,
-        ];
-        return view('login_EC.user_inf', $data);
+
+        return view('login_EC.user_inf', $user);
+    }
+
+    /**
+     * ユーザー情報変更ページ
+     */
+    public function name_edit(Request $request)
+    {
+        //ユーザー情報の取得
+        $user = new User();
+        $user = $user->getName($request);
+
+        return view('user_update.name_up', $user);
+    }
+
+    public function name_update(Request $request)
+    {
+        // /**バリデーション */
+        $this->validate($request, User::$name_rules);
+
+        $user = new User();
+        $user = $user->getName($request);
+        // dd($user);
+
+        /**formの内容を全て取得 */
+        $form = $request->all();
+
+        /**更新内容の保存 */
+        $user->fill($form)->save();
+        /**ユーザ情報ページにリダイレクト */
+        return redirect('/user_inf');
     }
 
     /**
@@ -33,40 +60,40 @@ class UserController extends Controller
     public function user_edit(Request $request)
     {
         //ユーザー情報の取得
-        $users = new User();
-        $users = $users->User_Data();
+        $user = new User();
+        $user = $user->User_Data();
 
         //都道府県情報の取得
         $prefectures = new Prefecture();
         $prefectures = $prefectures->getData();
 
         $data = [
-            'users' => $users,
+            'user' => $user,
             'prefectures' => $prefectures,
         ];
 
-        return view('Login_EC.user_up', $data);
+        return view('login_EC.user_up', $data);
     }
 
-    /**
-     * ユーザー情報変更処理
-     */
-    public function user_up(Request $request)
-    {
-        /**バリデーション */
-        $this->validate($request, User::$rules, User::$messages);
+    // /**
+    //  * ユーザー情報変更処理
+    //  */
+    // public function user_up(Request $request)
+    // {
+    //     /**バリデーション */
+    //     $this->validate($request, User::$rules, User::$messages);
 
-        /**変更するユーザ情報の確定 */
-        $auth = User::find(Auth::user()->id);
-        // dd($auth);
+    //     /**変更するユーザ情報の確定 */
+    //     $auth = User::find(Auth::user()->id);
+    //     // dd($auth);
 
-        /**formの内容を全て取得 */
-        $form = $request->all();
+    //     /**formの内容を全て取得 */
+    //     $form = $request->all();
 
-        /**更新内容の保存 */
-        $auth->fill($form)->save();
+    //     /**更新内容の保存 */
+    //     $auth->fill($form)->save();
 
-        /**ユーザ情報ページにリダイレクト */
-        return redirect('/login/main');
-    }
+    //     /**ユーザ情報ページにリダイレクト */
+    //     return redirect('/login/main');
+    // }
 }
