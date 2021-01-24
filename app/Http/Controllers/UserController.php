18c42096772989991fb6e16fdad6e82862b70d56
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-01-24 17:43:49 +0900

    アドレスと電話番号変更処理の作成

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,26 +19,23 @@ class UserController extends Controller
         //Userモデルからユーザ情報の取得
         $user = new User();
         $user = $user->User_Data();
-
-
-        return view('login_EC.user_inf', $user);
+        return view('user_update.user_inf', $user);
     }
 
-    /**
-     * ユーザー情報変更ページ
-     */
+    //ユーザー名変更ページへ移動
     public function name_edit(Request $request)
     {
         //ユーザー情報の取得
         $user = new User();
         $user = $user->getName($request);
 
-        return view('user_update.name_up', $user);
+        return view('user_update.name_edit', $user);
     }
 
+    //ユーザー名の変更を実行
     public function name_update(Request $request)
     {
-        // /**バリデーション */
+        // /**バリデーションの実行 */
         $this->validate($request, User::$name_rules);
 
         $user = new User();
@@ -53,6 +50,120 @@ class UserController extends Controller
         /**ユーザ情報ページにリダイレクト */
         return redirect('/user_inf');
     }
+
+    //パスワード変更ページへ移動
+    public function pass_edit(Request $request)
+    {
+        //ユーザー情報の取得
+        $user = new User();
+        $user = $user->getName($request);
+
+        return view('user_update.pass_edit', $user);
+    }
+
+    //ユーザー名の変更を実行
+    // public function pass_update(Request $request)
+    // {
+    //     // /**バリデーションの実行 */
+    //     $this->validate($request, User::$name_rules);
+
+    //     $user = new User();
+    //     $user = $user->getName($request);
+    //     // dd($user);
+
+    //     /**formの内容を全て取得 */
+    //     $form = $request->all();
+
+    //     /**更新内容の保存 */
+    //     $user->fill($form)->save();
+    //     /**ユーザ情報ページにリダイレクト */
+    //     return redirect('/user_inf');
+    // }
+
+    //メールアドレス変更ページへ移動
+    public function email_edit(Request $request)
+    {
+        //ユーザー情報の取得
+        $user = new User();
+        $user = $user->getName($request);
+
+        return view('user_update.email_edit', $user);
+    }
+    // メールアドレスの変更を実行
+    public function email_update(Request $request)
+    {
+        /**バリデーションの実行 */
+        $this->validate($request, User::$email_rules);
+
+        $user = new User();
+        $user = $user->getName($request);
+        // dd($user);
+
+        /**formの内容を全て取得 */
+        $form = $request->all();
+
+        /**更新内容の保存 */
+        $user->fill($form)->save();
+        /**ユーザ情報ページにリダイレクト */
+        return redirect('/user_inf');
+    }
+
+    //電話番号変更ページへ移動
+    public function phone_edit(Request $request)
+    {
+        //ユーザー情報の取得
+        $user = new User();
+        $user = $user->getName($request);
+
+        return view('user_update.phone_edit', $user);
+    }
+
+    //電話番号の変更を実行
+    public function phone_update(Request $request)
+    {
+        // /**バリデーションの実行 */
+        $this->validate($request, User::$phone_rules);
+
+        $user = new User();
+        $user = $user->getName($request);
+        // dd($user);
+
+        /**formの内容を全て取得 */
+        $form = $request->all();
+
+        /**更新内容の保存 */
+        $user->fill($form)->save();
+        /**ユーザ情報ページにリダイレクト */
+        return redirect('/user_inf');
+    }
+
+    //住所変更ページへ移動
+    public function address_edit(Request $request)
+    {
+        //ユーザー情報の取得
+        $user = new User();
+        $user = $user->getName($request);
+
+        return view('user_update.address_edit', $user);
+    }
+    // //住所の変更を実行
+    // public function address_update(Request $request)
+    // {
+    //     // /**バリデーションの実行 */
+    //     $this->validate($request, User::$name_rules);
+
+    //     $user = new User();
+    //     $user = $user->getName($request);
+    //     // dd($user);
+
+    //     /**formの内容を全て取得 */
+    //     $form = $request->all();
+
+    //     /**更新内容の保存 */
+    //     $user->fill($form)->save();
+    //     /**ユーザ情報ページにリダイレクト */
+    //     return redirect('/user_inf');
+    // }
 
     /**
      * ユーザー情報変更ページ
