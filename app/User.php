81b5a9592b30c941f062120a01c78cc6963780b1
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-01-24 16:25:37 +0900

    バリーデーションルールの作成

--- app/User.php
+++ app/User.php
@@ -7,6 +7,9 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+
 
 class User extends Authenticatable
 {
@@ -18,10 +21,24 @@ class User extends Authenticatable
     protected $fillable = ['prefecture_id', 'name', 'password', 'email', 'phone', 'postcode',  'city',  'block', 'building',];
 
     /**ユーザ情報更新用のバリデーション*/
-    public static $rules = [
-        'name' => ['required', 'string', 'between:7,8'],
+    //名前
+    public static $name_rules = [
+        'name' => ['required', 'string', 'between:2,8'],
+    ];
+    //パスワード
+    public static $pass_rules = [
+        'password' => ['required', 'string', 'min:8|max:30', 'confirmed'],
+    ];
+    //メール
+    public static $email_rules = [
         'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+    ];
+    //電話番号
+    public static $phone_rules = [
         'phone' => ['required', 'between:11,20'],
+    ];
+    //住所
+    public static $add_rules = [
         'postcode' => ['required', 'string', 'between:7,8'],
         'prefecture_id' => ['required', 'integer'],
         'city' => ['required', 'string', 'max:24'],
@@ -29,52 +46,56 @@ class User extends Authenticatable
         'building' => ['max:255'],
     ];
 
-    // /** バリデーションのエラーメッセージ*/
-    public static $messages = [
-        'name.required' => '名前を入力して下さい。',
-        'name.string' => '文字列で入力して下さい。',
-        'name.between' => '2文字以上30文字以内で入力してください',
+    /** バリデーションのエラーメッセージ*/
+    // public static $messages = [
+    //     'name.required' => '名前を入力して下さい。',
+    //     'name.string' => '文字列で入力して下さい。',
+    //     'name.between' => '2文字以上30文字以内で入力してください',
 
-        'password.required' => '必須項目です。',
-        'password.min' => '8文字以上で入力してください',
-        'password.max' => '30文字以内で入力してください',
-        'password.confirmed' => '確認欄と一致しません',
+    //     'password.required' => '必須項目です。',
+    //     'password.min' => '8文字以上で入力してください',
+    //     'password.max' => '30文字以内で入力してください',
+    //     'password.confirmed' => '確認欄と一致しません',
 
-        'email.required' => 'メールアドレスを入力して下さい。',
-        'email.string' => '文字列で入力して下さい。',
-        'email.email' => 'メール形式で入力して下さい。',
-        'email.unique' => 'すでにこのメールアドレスは登録されています。',
-        'email.max:255' => '255文字以内で入力して下さい。',
+    //     'email.required' => 'メールアドレスを入力して下さい。',
+    //     'email.string' => '文字列で入力して下さい。',
+    //     'email.email' => 'メール形式で入力して下さい。',
+    //     'email.unique' => 'すでにこのメールアドレスは登録されています。',
+    //     'email.max:255' => '255文字以内で入力して下さい。',
 
-        'phone.required' => '電話番号を入力して下さい。',
-        'phone.between' => '正しい電話番号を入力して下さい。',
+    //     'phone.required' => '電話番号を入力して下さい。',
+    //     'phone.between' => '正しい電話番号を入力して下さい。',
 
-        'postcode.required' => '郵便番号を入力して下さい。',
-        'postcode.string' => '文字列で入力して下さい。',
-        'postcode.between' => '正しい郵便番号を入力して下さい。',
+    //     'postcode.required' => '郵便番号を入力して下さい。',
+    //     'postcode.string' => '文字列で入力して下さい。',
+    //     'postcode.between' => '正しい郵便番号を入力して下さい。',
 
-        'prefecture_id.required' => '都道府県を選択して下さい。',
-        'prefecture_id.integer' => 'もう一度都道府県を選択しなおして下さい。',
+    //     'prefecture_id.required' => '都道府県を選択して下さい。',
+    //     'prefecture_id.integer' => 'もう一度都道府県を選択しなおして下さい。',
 
-        'city.required' => '市町村区を入力して下さい。',
-        'city.string' => '文字列で入力して下さい。',
-        'city.max' => '正しい住所を入力して下さい。',
+    //     'city.required' => '市町村区を入力して下さい。',
+    //     'city.string' => '文字列で入力して下さい。',
+    //     'city.max' => '正しい住所を入力して下さい。',
 
-        'block.required' => '番地を入力して下さい。',
-        'block.string' => '文字列で入力して下さい。',
-        'block.max' => '正しい住所を入力して下さい。',
+    //     'block.required' => '番地を入力して下さい。',
+    //     'block.string' => '文字列で入力して下さい。',
+    //     'block.max' => '正しい住所を入力して下さい。',
 
-        'building.max' => '正しい住所を入力して下さい。',
+    //     'building.max' => '正しい住所を入力して下さい。',
 
-    ];
+    // ];
 
-    /**
-     * prefecturesテーブルとのリレーション
-     * @return void
-     */
+    /**prefecturesテーブルとのリレーション*/
     public function prefecture()
     {
         return $this->hasMany('App\Models\Prefecture', 'id', 'name');
+    }
+
+    public function getName(Request $request)
+    {
+        $name = User::where('id', $request->id)
+            ->first();
+        return $name;
     }
 
     //ユーザー情報の取得
