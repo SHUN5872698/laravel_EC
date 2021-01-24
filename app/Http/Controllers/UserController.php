b9c24c85d6eb3d631955b7e1bf2b133321dd9532
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-01-24 11:01:48 +0900

    ユーザー情報高維新処理を追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
+use App\User;
+use App\Models\Prefecture;
 
 class UserController extends Controller
 {
@@ -18,11 +19,54 @@ class UserController extends Controller
     {
         //Userモデルからユーザ情報の取得
         $users = new User();
-        $users = $users->getUser();
+        $users = $users->User_Data();
 
         $data = [
             'users' => $users,
         ];
         return view('login_EC.user_inf', $data);
+    }
+
+    /**
+     * ユーザー情報変更ページ
+     */
+    public function user_edit(Request $request)
+    {
+        //ユーザー情報の取得
+        $users = new User();
+        $users = $users->User_Data();
+
+        //都道府県情報の取得
+        $prefectures = new Prefecture();
+        $prefectures = $prefectures->getData();
+
+        $data = [
+            'users' => $users,
+            'prefectures' => $prefectures,
+        ];
+
+        return view('Login_EC.user_up', $data);
+    }
+
+    /**
+     * ユーザー情報変更処理
+     */
+    public function user_up(Request $request)
+    {
+        /**バリデーション */
+        $this->validate($request, User::$rules, User::$messages);
+
+        /**変更するユーザ情報の確定 */
+        $auth = User::find(Auth::user()->id);
+        // dd($auth);
+
+        /**formの内容を全て取得 */
+        $form = $request->all();
+
+        /**更新内容の保存 */
+        $auth->fill($form)->save();
+
+        /**ユーザ情報ページにリダイレクト */
+        return redirect('/login/main');
     }
 }
