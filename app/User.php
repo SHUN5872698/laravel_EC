3347b6e73ee5b034cf48806c1bb678a347887254
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-01-25 11:43:11 +0900

    パスワード変更用のバリデーションを作成

--- app/User.php
+++ app/User.php
@@ -27,7 +27,8 @@ class User extends Authenticatable
     ];
     //パスワード
     public static $pass_rules = [
-        'password' => ['required', 'string', 'min:8|max:30', 'confirmed'],
+        'current_password' => ['required', 'password'],
+        'password' => ['required', 'string', 'between:8,30', 'confirmed'],
     ];
     //メール
     public static $email_rules = [
@@ -46,56 +47,10 @@ class User extends Authenticatable
         'building' => ['max:255'],
     ];
 
-    /** バリデーションのエラーメッセージ*/
-    // public static $messages = [
-    //     'name.required' => '名前を入力して下さい。',
-    //     'name.string' => '文字列で入力して下さい。',
-    //     'name.between' => '2文字以上30文字以内で入力してください',
-
-    //     'password.required' => '必須項目です。',
-    //     'password.min' => '8文字以上で入力してください',
-    //     'password.max' => '30文字以内で入力してください',
-    //     'password.confirmed' => '確認欄と一致しません',
-
-    //     'email.required' => 'メールアドレスを入力して下さい。',
-    //     'email.string' => '文字列で入力して下さい。',
-    //     'email.email' => 'メール形式で入力して下さい。',
-    //     'email.unique' => 'すでにこのメールアドレスは登録されています。',
-    //     'email.max:255' => '255文字以内で入力して下さい。',
-
-    //     'phone.required' => '電話番号を入力して下さい。',
-    //     'phone.between' => '正しい電話番号を入力して下さい。',
-
-    //     'postcode.required' => '郵便番号を入力して下さい。',
-    //     'postcode.string' => '文字列で入力して下さい。',
-    //     'postcode.between' => '正しい郵便番号を入力して下さい。',
-
-    //     'prefecture_id.required' => '都道府県を選択して下さい。',
-    //     'prefecture_id.integer' => 'もう一度都道府県を選択しなおして下さい。',
-
-    //     'city.required' => '市町村区を入力して下さい。',
-    //     'city.string' => '文字列で入力して下さい。',
-    //     'city.max' => '正しい住所を入力して下さい。',
-
-    //     'block.required' => '番地を入力して下さい。',
-    //     'block.string' => '文字列で入力して下さい。',
-    //     'block.max' => '正しい住所を入力して下さい。',
-
-    //     'building.max' => '正しい住所を入力して下さい。',
-
-    // ];
-
     /**prefecturesテーブルとのリレーション*/
     public function prefecture()
     {
         return $this->hasMany('App\Models\Prefecture', 'id', 'name');
-    }
-
-    public function getName(Request $request)
-    {
-        $name = User::where('id', $request->id)
-            ->first();
-        return $name;
     }
 
     //ユーザー情報の取得
@@ -109,14 +64,15 @@ class User extends Authenticatable
             ->select(
                 'users.id',
                 'users.prefecture_id',
-                'prefectures.name as prefectures_name',
                 'users.name',
                 'users.email',
+                'users.password',
                 'users.phone',
                 'users.postcode',
                 'users.city',
                 'users.block',
-                'users.building'
+                'users.building',
+                'prefectures.name as prefectures_name',
             )
             ->first();
         return $users;
