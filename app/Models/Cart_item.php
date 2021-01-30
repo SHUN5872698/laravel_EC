a40401d627f48e202b696e880a68e9c89266f047
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-01-30 10:50:46 +0900

    カート内の合計金額作成処理を作成

--- app/Models/Cart_item.php
+++ app/Models/Cart_item.php
@@ -4,37 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class Cart_item extends Model
 {
-    //タイムスタンプを無効化
+    /**タイムスタンプを無効化 */
     public $timestamps = false;
 
     /**ガードするフィールド */
     protected $guarded = array('id');
 
-    /**
-     * usersテーブルとのリレーション
-     * @return void
-     */
+    /**usersテーブルとのリレーション */
     public function user()
     {
         return $this->hasone('App\User', 'id');
     }
-    /**
-     * productsテーブルとのリレーション
-     * @return void
-     */
+
+    /**productsテーブルとのリレーション */
     public function product()
     {
         return $this->hasMany('App\Models\Product', 'id');
     }
-    /**
-     * productimagesテーブルとのリレーション
-     * @return void
-     */
+    /**productimagesテーブルとのリレーション */
     public function productimage()
     {
         return $this->hasMany('App\Models\Productimage', 'product_id');
@@ -42,12 +33,14 @@ class Cart_item extends Model
 
     /**
      * カート情報の取得
-     * @return void
+     *
+     *@return void
+     * ログインしているユーザーのidからcart情報を抽出
+     * 商品に紐づいている商品情報やメイン画像を抽出
      */
-    public function getCart_items()
+    public function Cart_items()
     {
-        $cart_items = Cart_item::with('User', 'Product', 'Productimage')
-            ->join('users', 'users.id', '=', 'cart_items.user_id')
+        $cart_items = Cart_item::with('Product', 'Productimage')
             ->join('products', 'products.id', '=', 'cart_items.product_id')
             ->join('productimages', 'productimages.product_id', '=', 'products.id')
             ->where('cart_items.user_id', Auth::user()->id)
@@ -66,36 +59,39 @@ class Cart_item extends Model
             )
             ->orderby('cart_items.id', 'asc')
             ->paginate(12);
-
         return $cart_items;
     }
+
     /**
-     * 購入確認のデータ取得
+     * カート内商品の合計金額の作成
+     *
+     * @return void
      */
-    public function getOrder_Check()
+    public function totalprice()
     {
-        $order_check = Cart_item::with('User', 'Product')
-            ->join('users', 'users.id', '=', 'cart_items.user_id')
+        $totalprice = 0;
+        $items = Cart_item::with('Product')
             ->join('products', 'products.id', '=', 'cart_items.product_id')
             ->where('cart_items.user_id', Auth::user()->id)
             ->select(
-                'cart_items.id',
-                'cart_items.user_id',
-                'cart_items.product_id',
                 'cart_items.count',
-                'products.name',
                 'products.price',
             )
-            ->orderby('cart_items.id', 'asc')
-            ->paginate(12);
-
-        return $order_check;
+            ->get();
+        foreach ($items as $item) {
+            $totalprice += $item->price  * $item->count;
+        }
+        return $totalprice;
     }
+
+
 
     /**
      *order_itemsテーブルに登録する商品情報の取得
+     *
      * @param Request $request
      * @return void
+     * 商品名と価格をproductsテーブルから抽出
      */
     public function products_confirmed(Request $request)
     {
@@ -116,7 +112,7 @@ class Cart_item extends Model
     }
 
     /**
-     *該当ユーザのcart情報を削除
+     * 該当ユーザのcart情報を削除
      * @param Request $request
      * @return void
      */
