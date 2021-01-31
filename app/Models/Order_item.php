d07c042ad6a669119c7fcb1781cf6c16be2677a7
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-01-31 16:37:46 +0900

    購入時の登録処理を変更

--- app/Models/Order_item.php
+++ app/Models/Order_item.php
@@ -12,69 +12,60 @@ class Order_item extends Model
     protected $guarded = array('id');
 
     /**ホワイトリスト */
-    protected $fillable = ['product_id', 'order_id', 'name', 'price', 'count'];
+    // protected $fillable = ['product_id', 'order_id', 'name', 'price', 'count'];
 
-    /**
-     * orderテーブルとのリレーション
-     * @return void
-     */
+    /** orderテーブルとのリレーション*/
     public function order()
     {
         return $this->belongsTo('App\Models\Order', 'id');
     }
 
-    /**
-     * productsテーブルとのリレーション
-     * @return void
-     */
+    /** productsテーブルとのリレーション*/
     public function product()
     {
         return $this->hasMany('App\Models\Product', 'id');
     }
 
-
+    /** productimagesテーブルとのリレーション*/
     public function productimage()
     {
         return $this->hasMany('App\Models\Productimage', 'product_id');
     }
 
-
-    /**
-     * cart_itemsテーブルとのリレーション
-     * @return void
-     */
+    /**cart_itemsテーブルとのリレーション*/
     public function cart_item()
     {
         return $this->hasMany('App\Models\Product', 'product_id', 'count');
     }
 
     /**
-     *新規レコードの作成処理
+     * 新規レコードの作成処理
      * @param Request $request
      * @return void
+     * カート情報と該当idを抽出して登録処理を行う
      */
-    public function  items_confirmed(Request $request)
+    public function  Items_add()
     {
         //登録する商品情報の取得
-        $products = new Cart_item();
-        $products = $products->products_confirmed($request);
+        $items = new Cart_item();
+        $items = $items->Items();
 
-        //登録するorder_idの取得
+        //登録するorder_idを取得
         $order_id = new Order();
-        $order_id = $order_id->getOrder_User($request);
+        $order_id = $order_id->Order_id();
 
         //レコードの作成
-        foreach ($products as $product) {
-            $order_confirmed = Order_item::create(
+        foreach ($items as $item) {
+            $order_add = Order_item::create(
                 [
-                    'product_id' => $product->product_id,
+                    'product_id' => $item->product_id,
                     'order_id' => $order_id->id,
-                    'name' => $product->name,
-                    'price' => $product->price,
-                    'count' => $product->count,
+                    'name' => $item->name,
+                    'price' => $item->price,
+                    'count' => $item->count,
                 ],
             );
         }
-        return ($order_confirmed);
+        return ($order_add);
     }
 }
