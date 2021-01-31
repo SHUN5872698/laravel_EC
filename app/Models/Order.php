b3d1107379b11789113e9f9830e19a0f8c110555
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-01-31 16:37:27 +0900

    購入時の登録処理を変更

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,90 +5,83 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
+use App\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
+
 class Order extends Model
 {
-    //タイムスタンプを無効化
+    /** タイムスタンプを無効化 */
     public $timestamps = false;
 
     /**ガードするフィールド */
     protected $guarded = array('id');
 
+    /** date型にキャスト */
     protected $dates = ['order_date'];
 
-    // protected $casts = ['total_price' => 'int'];
+    /** ホワイトリスト */
+    // protected $fillable = ['user_id', 'prefecture_id', 'total_price', 'order_date', 'name', 'postcode', 'city', 'postcode', 'block', 'building', 'tax'];
 
-    /**ホワイトリスト */
-    protected $fillable = ['user_id', 'prefecture_id', 'total_price', 'order_date', 'name', 'postcode', 'city', 'postcode', 'block', 'building', 'tax'];
-
-
-
-    // /**
-    //  * usersテーブルとのリレーション
-    //  * @return void
-    //  */
-    // public function user()
-    // {
-    //     return $this->hasone('App\User', 'id');
-    // }
-    /**
-     * prefectureテーブルとのリレーション
-     * @return void
-     */
+    /** prefecturesテーブルとのリレーション */
     public function prefecture()
     {
         return $this->hasone('App\Models\Prefecture', 'id');
     }
 
-    /**
-     * productimagesテーブルとのリレーション
-     * @return void
-     */
+    /** productimagesテーブルとのリレーション */
     public function productimage()
     {
         return $this->hasMany('App\Models\Productimage', 'product_id');
     }
 
-    /**
-     * order_itemsテーブルとのリレーション
-     * @return void
-     */
+    /** order_itemsテーブルとのリレーション */
     public function order_item()
     {
         return $this->hasmany('App\Models\Order_item', 'order_id');
     }
 
     /**
-     *新規レコードの作成処理
+     * 新規レコードの作成処理
      * @param Request $request
      * @return void
+     *
+     * 今日の日付を取得
+     * 購入者情報と現在の税率、カート内合計金額を抽出
+     * 合計金額は税率混みの金額に変更処理を行い登録する
      */
-    public function order_confirmed(Request $request)
+    public function Order_add()
     {
-        //登録するユーザー情報の取得
-        $users = new User();
-        $users = $users->order_confirmed($request);
+        //ユーザー情報をログインしているユーザーから取得
+        $auth = User::find(Auth::user()->id);
 
-        //登録する税率の取得
+        //税率の取得
         $tax = new Tax();
         $tax = $tax->getTax();
+
+        //カート内商品の合計金額を取得
+        $total_price = new Cart_item();
+        $total_price = $total_price->Total_price();
+
+        //合計金額を税込価格に変換
+        $total_price = $total_price * $tax->percentage;
 
         //購入日の作成
         $now = Carbon::now()->format('Y-m-d');
 
+        //登録処理の実行
         $order_confirmed = Order::create(
             [
-                'user_id' => $users->id,
-                'prefecture_id' => $users->prefecture_id,
-                'total_price' => $request->total_price,
+                'user_id' => $auth->id,
+                'prefecture_id' => $auth->prefecture_id,
+                'total_price' => $total_price,
                 'order_date' => $now,
-                'name' => $users->name,
-                'postcode' => $users->postcode,
-                'city' => $users->city,
-                'block' => $users->block,
-                'building' => $users->building,
+                'name' => $auth->name,
+                'postcode' => $auth->postcode,
+                'city' => $auth->city,
+                'block' => $auth->block,
+                'building' => $auth->building,
                 'tax' => $tax->percentage,
             ],
         );
@@ -96,25 +89,27 @@ class Order extends Model
     }
 
     /**
-     *order_itemsテーブルに登録するorder_id情報を取得
-     * @param Request $request
+     * order_id情報を取得
      * @return void
+     * order_itemsテーブルに登録するので最新の物を取得
      */
-    public function getOrder_User(Request $request)
+    public function Order_id()
     {
-        $getrder_User = Order::where('user_id', $request->user_id)
+        $user = Order::where('user_id', Auth::user()->id)
             ->select(
                 'orders.id',
             )
             ->orderby('id', 'desc')
             ->first();
-        return ($getrder_User);
+        return ($user);
     }
 
     /**
      *購入履歴情報の取得
-     * @param Request $request
      * @return void
+     *
+     * ログインしているユーザーidから購入者情報を購入日が新しい順に抽出
+     * 抽出したidから購入した商品情報を取得して同一の連想配列内に格納
      */
     public function Order_History()
     {
@@ -135,11 +130,14 @@ class Order extends Model
                 'orders.building',
                 'orders.tax',
             )
-            ->paginate(12);
+            ->get();
 
+        //繰り返す変数の初期化
         $i = 0;
+        //格納する連想配列を作成
         $orders = array();
-        //orders.idから購入商品の情報を取得
+
+        //購入商品の情報を取得
         foreach ($users as $user) {
             $items = Order_item::with('Productimage')
                 ->join('products', 'products.id', '=', 'order_items.product_id')
@@ -154,11 +152,14 @@ class Order extends Model
                     'order_items.price',
                     'order_items.count',
                 )
-                ->paginate(12);
+                ->get();
+
+            //購入者情報と購入商品を連想配列に格納する。
             $orders[$i]['order'] = $user;
             $orders[$i]['item'] = $items;
             $i++;
         }
+
         return $orders;
     }
 }
