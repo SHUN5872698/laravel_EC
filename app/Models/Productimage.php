5a61956f74c556c4f9124c0a5d2bee2b6239882a
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-02-03 07:07:59 +0900

    画像の取得処理を作成

--- app/Models/Productimage.php
+++ app/Models/Productimage.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Productimage extends Model
 {
-    //
+    /** productsテーブルとのリレーション*/
+    public function product()
+    {
+        return $this->belongsTo('App\Models\Product', 'id');
+    }
+
+    /**
+     * 商品画像の取得
+     * @param Request $request
+     * @return void
+     * 該当商品の画像をid順に並び替えて抽出
+     */
+    public function Images(Request $request)
+    {
+        $images = Productimage::where('product_id', $request->id)
+            ->orderBy('id', 'asc')
+            ->pluck('image');
+        return $images;
+    }
 }
