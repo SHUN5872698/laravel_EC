725362236ca3e755dcf221b38d2d8e67c7508a39
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-02-03 11:14:21 +0900

    レイアウトの変更

--- resources/views/EC/main.blade.php
+++ resources/views/EC/main.blade.php
@@ -5,24 +5,29 @@
 @section('menubar')
 
 @section('content')
-<h1>メインページ</h1>
+<div class="row">
+    <div class="col-md-3">
+        <h1 class="ml-3">メインページ</h1>
+    </div>
+    <div class="col-md-9"></div>
+    <div class="col-md-3"></div>
+</div>
 <div class="row">
     @foreach ($products as $product)
     <div class="col-md-3 my-3">
-        <div class="card mx-3">
+        <div class="card mx-2">
             <img src="../images/{{$product->image}}" class="card-img-top my-3" alt="..." width="300" height="300">
             <div class="card-body">
-                <p>
+                <p class="product-card">
                     <font size="2"><a href="/product?id={{$product->id}}&category_master={{$product->category_master}}">{{$product->name}}</a></font>
                     <br>
                     価格:
                     <font color="red">
-                        ¥{{number_format($product->price * $tax->percentage)}}円</font>
+                        ¥{{number_format($product->price * $tax)}}円</font>
                     <font size="2" color="red">(税込)</font>
                     <br>
                     スペック:
                     <a href="/details/category?category={{$product->category}}&category_master={{$product->category_master}}"> {{$product->category}}</a>
-
                 </p>
             </div>
         </div>
--- resources/views/login_EC/login_main.blade.php
+++ resources/views/login_EC/login_main.blade.php
@@ -5,32 +5,35 @@
 @section('menubar')
 
 @section('content')
-<h1>ログイン済みのメインページ</h1>
+<div class="row">
+    <div class="col-md-3">
+        <h1 class="ml-3">メインページ</h1>
+    </div>
+    <div class="col-md-9"></div>
+    <div class="col-md-3"></div>
+</div>
 <div class="row">
     @foreach ($products as $product)
     <div class="col-md-3 my-3">
-        <div class="card mx-3">
+        <div class="card mx-1">
             <img src="../images/{{$product->image}}" class="card-img-top my-3" alt="..." width="300" height="300">
-            <div class="row">
-                <div class="card-body ml-1">
-                    <p class="product-card">
-                        <font size="2"><a href="/login/product?id={{$product->id}}&category_master={{$product->category_master}}">{{$product->name}}</a></font>
-                        <br>
-                        価格:
-                        <font color="red">
-                            ¥{{number_format($product->price * $tax->percentage)}}円</font>
-                        <font size="2" color="red">(税込)</font>
-                        <br>
-                        スペック:
-                        <a href="/login/details/category?category={{$product->category}}&category_master={{$product->category_master}}"> {{$product->category}}</a>
-                    </p>
-                </div>
+            <div class="card-body">
+                <p class="product-card">
+                    <font size="2"><a href="/login/product?id={{$product->id}}&category_master={{$product->category_master}}">{{$product->name}}</a></font>
+                    <br>
+                    価格:
+                    <font color="red">
+                        ¥{{number_format($product->price * $tax)}}円</font>
+                    <font size="2" color="red">(税込)</font>
+                    <br>
+                    スペック:
+                    <a href="/login/details/category?category={{$product->category}}&category_master={{$product->category_master}}"> {{$product->category}}</a>
+                </p>
             </div>
         </div>
     </div>
     @endforeach
 </div>
 @endsection
-
 @yield('footer')
 @endsection
