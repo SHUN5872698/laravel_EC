475b865d16ded6ad57f36db2d58408659b237a4c
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-02-04 06:50:07 +0900

    検索項目を別カードに分ける、検索された内容、件数を表示する

--- resources/views/login_EC/login_details.blade.php
+++ resources/views/login_EC/login_details.blade.php
@@ -5,19 +5,43 @@
 @section('menubar')
 
 @section('content')
-<h1>ログイン済み詳細検索</h1>
+@if (isset($one_category))
+<div class="row">
+    <div class="col-md-12">
+        <h1 class="ml-3">
+            <font color="darkorange">"{{$one_category}}"</font>
+            の商品一覧
+        </h1>
+    </div>
+</div>
+@elseif(isset($one_capacity))
+<div class="row">
+    <div class="col-md-12">
+        <h1 class="ml-3">
+            <font color="darkorange">"{{$one_capacity->category}}"</font>
+            の商品一覧
+        </h1>
+    </div>
+</div>
+@endif
 <div class="row">
     <div class="col-md-3">
-        <div class="card ml-3 mt-3">
-            <div class="card-header">詳細検索</div>
+        <div class="card ml-3 mb-3">
+            <div class="card-header">スペックで絞り込み</div>
             <ul class="list-group list-group-flush">
-                <text class="ml-3 mt-2">スペックで選択</text>
+                <text class="ml-3 mt-2"></text>
                 <li class="list-group-item">
                     @foreach ($categorys as $category)
                     <a href="/login/details/category?category={{$category->category}}&category_master={{$category->category_master}}">{{$category->category}}</a>
                     <br>
-                    @endforeach</li>
-                <text class="ml-3 mt-2">容量で選択</text>
+                    @endforeach
+                </li>
+            </ul>
+        </div>
+        <div class="card ml-3">
+            <div class="card-header">容量で絞り込み</div>
+            <ul class="list-group list-group-flush">
+                <text class="ml-3 mt-2"></text>
                 <li class="list-group-item">
                     @foreach ($capacitys as $capacity)
                     <a href="/login/details/capacity?category_master={{$capacity->category_master}}&category={{$capacity->category}}
@@ -29,33 +53,47 @@
             </ul>
         </div>
     </div>
+    <div class="col-md-9">
+        <div class="card">
+            <div class="card-header">
+                @if (isset($one_category))
+                <font class="one_category" color="darkorange">"{{$one_category}}"</font>
+                で{{$count}}件ヒット
+                @elseif(isset($one_capacity))
+                <font class="one_capacity" color="darkorange">"{{$one_capacity->category}}"</font>,
+                <font class="one_capacity" color="deepskyblue">"{{$one_capacity->capacity}}"</font>,
+                で{{$count}}件ヒット
 
-    <div class="col-md-8 mx-4">
-        <div class="row">
-            @foreach ($products as $product)
-            <div class="col-md-4 my-3">
-                <div class="card">
-                    <img src="/images/{{$product->image}}" class="card-img-top my-3" alt="..." width="250" height="280">
-                    <div class="card-body">
-                        <div class="row">
-                            <p>
-                                <font size="2"><a href="/login/product?id={{$product->id}}&category_master={{$product->category_master}}">{{$product->name}}</a></font>
-                                <br>
-                                価格:
-                                <font size="3" color="red">
-                                    ¥{{number_format($product->price * $tax->percentage)}}</font>
-                                <font size="2" color="red">(税込)</font>
-                                <br>
-                                スペック:
-                                <a href="/login/details/category?category={{$product->category}}&category_master={{$product->category_master}}"> {{$product->category}}</a>
-                            </p>
+                @endif
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    @foreach ($products as $product)
+                    <div class="col-md-4 mb-1">
+                        <div class="card-body">
+                            <img src="/images/{{$product->image}}" class="card-img-top my-3" alt="..." width="250" height="280">
+                            <div class="row my-2">
+                                <p>
+                                    <font size="2"><a href="/login/product?id={{$product->id}}&category_master={{$product->category_master}}">{{$product->name}}</a></font>
+                                    <br>
+                                    価格:
+                                    <font size="3" color="red">
+                                        ¥{{number_format($product->price * $tax)}}</font>
+                                    <font size="2" color="red">(税込)</font>
+                                    <br>
+                                    スペック:
+                                    <a href="/login/details/category?category={{$product->category}}&category_master={{$product->category_master}}"> {{$product->category}}</a>
+                                </p>
+                            </div>
                         </div>
                     </div>
+                    @endforeach
                 </div>
             </div>
-            @endforeach
         </div>
-        {{$products->appends(request()->query())->links()}}
+        <div class="links">
+            {{$products->appends(request()->query())->links()}}
+        </div>
     </div>
 </div>
 
