f11de5dc17a5a32bbd077f5466561f8431e2656a
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-02-04 06:48:49 +0900

    レイアウトを未ログインの検索画面と同じに変更

--- resources/views/login_EC/login_search.blade.php
+++ resources/views/login_EC/login_search.blade.php
@@ -5,18 +5,18 @@
 
 @section('content')
 <div class="row">
-    <div class="col-md-4">
-        @foreach ($products as $product)
-        @endforeach
-        <h1 class="ml-3">"{{$product->category_master}}"の検索結果</h1>
+    <div class="col-md-12">
+        <h1 class="ml-3">
+            <font color="darkorange">"{{$one_master}}"</font>
+            の商品一覧
+        </h1>
     </div>
 </div>
 <div class="row my-3">
     <div class="col-md-3">
         <div class="card ml-3">
-            <div class="card-header">検索項目</div>
+            <div class="card-header">スペックで絞り込む</div>
             <ul class="list-group list-group-flush">
-                <text class="ml-3 mt-2">スペックで選択</text>
                 <li class="list-group-item">
                     @foreach ($categorys as $category)
                     <a href="/login/details/category?category={{$category->category}}&category_master={{$category->category_master}}">{{$category->category}}</a>
@@ -29,7 +29,7 @@
     <div class="col-md-9">
         <div class="card ml-">
             <div class="card-header">
-                <font class="one_master" color="red">"{{$one_master}}"</font>
+                <font class="one_master" color="darkorange">"{{$one_master}}"</font>
                 で{{$count}}件ヒット
             </div>
             <div class="card-body">
