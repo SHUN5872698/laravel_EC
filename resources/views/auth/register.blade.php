5bb43a9d79c6182e8ec4c562d27bd83d787cdd11
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-02-22 14:18:09 +0900

    都道府県欄のコメントアウトを解除

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -117,87 +117,87 @@
                                 <font color="red">必須</font>
                             </label>
 
-                            {{-- <div class="col-md-6 mt-2">
+                            <div class="col-md-6 mt-2">
                                 <select name="prefecture_id" id="inputAddress02">
                                     <option value="">--- 選択してください ---</option>
                                     @foreach ($prefectures as $prefecture)
                                     <option value={{$prefecture->id}} @if(old('prefecture_id')==$prefecture->id ) selected @endif>
-                            {{$prefecture->name}}</option>
-                            @endforeach
-                            </select>
+                                        {{$prefecture->name}}</option>
+                                    @endforeach
+                                </select>
 
-                            @error('prefecture_id')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
+                                @error('prefecture_id')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
                         </div>
-                </div> --}}
 
-                <div class="form-group row">
-                    <label for="inputAddress03" class="col-md-4 col-form-label text-md-right">
-                        <font class="mx-2">{{ __('市町村区') }}</font>
-                        <font color="red">必須</font>
-                    </label>
+                        <div class="form-group row">
+                            <label for="inputAddress03" class="col-md-4 col-form-label text-md-right">
+                                <font class="mx-2">{{ __('市町村区') }}</font>
+                                <font color="red">必須</font>
+                            </label>
 
-                    <div class="col-md-6">
-                        <input id="inputAddress03" type="text" name="city" class="form-control" value="{{ old('city') }}" required autocomplete="new-city">
+                            <div class="col-md-6">
+                                <input id="inputAddress03" type="text" name="city" class="form-control" value="{{ old('city') }}" required autocomplete="new-city">
 
-                        @error('city')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
+                                @error('city')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="inputAddress04" class="col-md-4 col-form-label text-md-right">
+                                <font class="mx-2">{{ __('番地') }}</font>
+                                <font color="red">必須</font>
+                            </label>
+
+                            <div class="col-md-6">
+                                <input id="inputAddress04" type="text" name="block" class="form-control" value="{{ old('block') }}" required autocomplete="new-block">
+
+                                @error('block')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="inputAddress05" class="col-md-4 col-form-label text-md-right">
+                                <font class="">{{ __('建物名・部屋番号') }}</font>
+                                <font></font>
+                            </label>
+
+                            <div class="col-md-6">
+                                <input id="inputAddress05" type="text" name="building" class="form-control" value="{{ old('building') }}">
+
+                                @error('building')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                                <button type="button" class="btn btn-outline-primary" onclick="location.href='/main';">キャンセル</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-
-                <div class="form-group row">
-                    <label for="inputAddress04" class="col-md-4 col-form-label text-md-right">
-                        <font class="mx-2">{{ __('番地') }}</font>
-                        <font color="red">必須</font>
-                    </label>
-
-                    <div class="col-md-6">
-                        <input id="inputAddress04" type="text" name="block" class="form-control" value="{{ old('block') }}" required autocomplete="new-block">
-
-                        @error('block')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </div>
-
-                <div class="form-group row">
-                    <label for="inputAddress05" class="col-md-4 col-form-label text-md-right">
-                        <font class="">{{ __('建物名・部屋番号') }}</font>
-                        <font></font>
-                    </label>
-
-                    <div class="col-md-6">
-                        <input id="inputAddress05" type="text" name="building" class="form-control" value="{{ old('building') }}">
-
-                        @error('building')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </div>
-
-                <div class="form-group row mb-0">
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="btn btn-primary">
-                            {{ __('Register') }}
-                        </button>
-                        <button type="button" class="btn btn-outline-primary" onclick="location.href='/main';">キャンセル</button>
-                    </div>
-                </div>
-                </form>
             </div>
         </div>
     </div>
-</div>
-<script src="https://ajaxzip3.github.io/ajaxzip3.js" charset="UTF-8"></script>
+    <script src="https://ajaxzip3.github.io/ajaxzip3.js" charset="UTF-8"></script>
 </div>
 @endsection
