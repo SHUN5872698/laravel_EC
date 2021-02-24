1e261da3c887896b9b7091e566ca67f672861aa7
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-02-24 15:49:55 +0900

    スタートページをコメントアウト、Authを元に戻す

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,12 @@
 
 use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
-// Auth::routes();
+Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
