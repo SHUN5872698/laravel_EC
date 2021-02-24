d4eacf2ad362eea05afa6d43e352315f9fbb38a8
SHUNSUKE FUJII <tekisasu.bager@gmail.com>
2021-02-24 15:53:24 +0900

    no message

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,7 @@ use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
+Auth::routes();
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -40,7 +39,7 @@ Route::get('/details/category', 'ProductController@category');
 //capacityで絞り込み
 Route::get('/details/capacity', 'ProductController@capacity');
 
-// Auth::routes();
+
 
 Route::get('/home', 'HomeController@index')->name('home');
 
