8a8df765b660336e6cdf6c8c780ffad5985ccd42
SHUN5872698 <tekisasu.bager@gmail.com>
2021-04-06 10:48:04 +0900

    S3に保存している画像データを表示できるように変更

--- resources/views/login_EC/order_check.blade.php
+++ resources/views/login_EC/order_check.blade.php
@@ -57,8 +57,8 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="image">
-                            <img src="../images/{{$item->image}}" width="100" height="100">
-                            {{--  <img src="https://test-bucket5842.s3-ap-northeast-1.amazonaws.com/laravel_EC/{{$item->image}}" width="150" height="150"> --}}
+                            {{-- <img src="../images/{{$item->image}}" width="100" height="100"> --}}
+                            <img src="https://test0123-bucket.s3.ap-northeast-3.amazonaws.com/images/{{$item->image}}" width="150" height="150">
                         </div>
                         <div class="product">
                             <div class="product_name">
