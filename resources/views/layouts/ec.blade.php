15b0bcb3df843a28ce37433cf296b5adfc534a94
SHUN5872698 <tekisasu.bager@gmail.com>
2021-04-06 12:16:51 +0900

    fixme.jsの読み込み

--- resources/views/layouts/ec.blade.php
+++ resources/views/layouts/ec.blade.php
@@ -19,6 +19,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="/assets/js/fixme.js"></script>
     @yield('css')
     <style>
     </style>
