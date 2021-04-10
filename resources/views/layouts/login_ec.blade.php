fc173b24bb5ebde467c23d41eb23318c436eee3d
SHUN5872698 <tekisasu.bager@gmail.com>
2021-04-10 14:49:40 +0900

    no message

--- resources/views/layouts/login_ec.blade.php
+++ resources/views/layouts/login_ec.blade.php
@@ -11,7 +11,6 @@
     <title>@yield('title')</title>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ asset('js/fixme.js') }}"></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
